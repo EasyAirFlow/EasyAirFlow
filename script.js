b8b5408264882f0a8e7362da04894e53e8b20217
EasyAirFlow <tawbusinessmail@gmail.com>
2024-12-02 20:09:25 +0100

    New version

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 
 document.getElementById('buy-now').addEventListener('click', function() {
-    alert('Thank you for your purchase!');
+    alert('Thank you for your interest! Please proceed to checkout.');
 });
