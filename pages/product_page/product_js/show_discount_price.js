6b41344c5df2f1e7f8d8e598d73a03188858f587
EasyAirFlow <tawbusinessmail@gmail.com>
2024-12-29 11:09:47 +0100

    <

--- pages/product_page/product_js/show_discount_price.js
+++ pages/product_page/product_js/show_discount_price.js
@@ -75,7 +75,7 @@ async function applyDiscount() {
       const discountedPrice =
         originalPrice - (originalPrice * discountPercentage) / 100;
       priceElement.innerHTML = `
-    <span class="old-price">$${originalPrice.toFixed(2)}/span>
+    <span class="old-price">$${originalPrice.toFixed(2)}</span>
     <br> -${discountPercentage}%</br>
     <span class="new-price">$${discountedPrice.toFixed(2)}</span>
     `;
