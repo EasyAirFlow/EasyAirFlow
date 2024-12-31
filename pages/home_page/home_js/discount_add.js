46c1b63c515c2591af06921470a09be6185ed48b
EasyAirFlow <tawbusinessmail@gmail.com>
2024-12-31 15:20:02 +0100

    Update discount_add.js

--- pages/home_page/home_js/discount_add.js
+++ pages/home_page/home_js/discount_add.js
@@ -67,9 +67,8 @@ async function validateDiscountCode(event) {
           setCookie("discountCode", discountCode, 30);
 
           // Display "Applying Discount..." and redirect after 3 seconds 
-        setTimeout(() => {feedbackMessage.textContent = "Applying Discount 3s..."; feedbackMessage.style.color = "blue";}, 2000);
-        setTimeout(() => {feedbackMessage.textContent = "Applying Discount 2s..."; feedbackMessage.style.color = "blue";}, 2000);
-        setTimeout(() => {feedbackMessage.textContent = "Applying Discount 1s..."; feedbackMessage.style.color = "blue";}, 2000);
+        setTimeout(() => {feedbackMessage.textContent = "Applying Discount 3s..."; feedbackMessage.style.color = "blue"; setTimeout(() => {feedbackMessage.textContent = "Applying Discount 2s..."; feedbackMessage.style.color = "blue"; setTimeout(() => {feedbackMessage.textContent = "Applying Discount 1s..."; feedbackMessage.style.color = "blue";}, 2000);}, 2000);}, 2000);
+        
         // Redirect to the product page
         window.location.href = "/pages/product_page/";
         
